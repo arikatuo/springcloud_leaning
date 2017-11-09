d88f5ef22b8455347742aaa494241c138a1986ef
sumimasah <superonlyme@outlook.com>
2017-11-09 10:13:11 +0800

    服务消费者（feign）

---
+++ chapter_2/serice-feign/src/main/java/springcloud_learning/HiController.java
@@ -0,0 +1,21 @@
+package springcloud_learning;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sumimasah on 2017/11/9.
+ */
+@RestController
+public class HiController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
