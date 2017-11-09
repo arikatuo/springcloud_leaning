c99105629ac19589c28ed4cebebd304ccc987dc1
sumimasah <superonlyme@outlook.com>
2017-11-09 14:24:55 +0800

    高可用的分布式配置中心(Spring Cloud Config)

--- chapter_3/cloud_config-server/src/main/java/springcloud_learnnig/CloudConfigServerApplication.java
+++ chapter_3/cloud_config-server/src/main/java/springcloud_learnnig/CloudConfigServerApplication.java
@@ -3,9 +3,12 @@ package springcloud_learnnig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class CloudConfigServerApplication {
 
 	public static void main(String[] args) {
