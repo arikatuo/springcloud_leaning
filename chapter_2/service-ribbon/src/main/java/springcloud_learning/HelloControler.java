3e4327ced4d102589c5cd5a2520badcc221e6d11
sumimasah <superonlyme@outlook.com>
2017-11-09 10:04:34 +0800

    服务消费者（rest+ribbon）

---
+++ chapter_2/service-ribbon/src/main/java/springcloud_learning/HelloControler.java
@@ -0,0 +1,22 @@
+package springcloud_learning;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sumimasah on 2017/11/9.
+ */
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+
+
+}
